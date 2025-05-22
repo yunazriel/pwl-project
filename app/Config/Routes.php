a8b870788e260724a2c3a085083cd3a1d669fae8
yunazriel <azrielazriel09@gmail.com>
2025-05-22 09:55:37 +0700

    Create-Update-dan-Delete-Data

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,10 +5,18 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('/', 'Home::index', ['filter' => 'auth']);
 $routes->get('/produk', 'ProdukController::index', ['filter' => 'auth']);
 $routes->get('/keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 $routes->get('/pesanan', 'PesananController::index', ['filter' => 'auth']);
+
+$routes->group('produk', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'ProdukController::index');
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+});
 
 $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login', ['filter' => 'redirect']);
--- app/Controllers/ProdukController.php
+++ app/Controllers/ProdukController.php
@@ -3,12 +3,82 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\ProductModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
-class ProdukController extends BaseController
-{
-    public function index()
-    {
-        return view('v_produk');
+class ProdukController extends BaseController {
+    protected $product; 
+
+    function __construct(){
+        $this->product = new ProductModel();
+    }
+
+    public function edit($id) {
+        $dataProduk = $this->product->find($id);
+
+        $dataForm = [
+            'nama' => $this->request->getPost('nama'),
+            'harga' => $this->request->getPost('harga'),
+            'jumlah' => $this->request->getPost('jumlah'),
+            'updated_at' => date("Y-m-d H:i:s")
+        ];
+
+        if ($this->request->getPost('check') == 1) {
+            if ($dataProduk['foto'] != '' and file_exists("img/" . $dataProduk['foto'] . "")) {
+                unlink("img/" . $dataProduk['foto']);
+            }
+
+            $dataFoto = $this->request->getFile('foto');
+
+            if ($dataFoto->isValid()) {
+                $fileName = $dataFoto->getRandomName();
+                $dataFoto->move('img/', $fileName);
+                $dataForm['foto'] = $fileName;
+            }
+        }
+
+        $this->product->update($id, $dataForm);
+
+        return redirect('produk')->with('success', 'Data Berhasil Diubah');
+    }
+
+    public function delete($id) {
+        $dataProduk = $this->product->find($id);
+
+        if ($dataProduk['foto'] != '' and file_exists("img/" . $dataProduk['foto'] . "")) {
+            unlink("img/" . $dataProduk['foto']);
+        }
+
+        $this->product->delete($id);
+
+        return redirect('produk')->with('success', 'Data Berhasil Dihapus');
+    }
+
+    public function create() {
+        $dataFoto = $this->request->getFile('foto');
+
+        $dataForm = [
+            'nama' => $this->request->getPost('nama'),
+            'harga' => $this->request->getPost('harga'),
+            'jumlah' => $this->request->getPost('jumlah'),
+            'created_at' => date("Y-m-d H:i:s")
+        ];
+
+        if ($dataFoto->isValid()) {
+            $fileName = $dataFoto->getRandomName();
+            $dataForm['foto'] = $fileName;
+            $dataFoto->move('img/', $fileName);
+        }
+
+        $this->product->insert($dataForm);
+
+        return redirect('produk')->with('success', 'Data Berhasil Ditambah');
+    } 
+
+    public function index() {
+        $product = $this->product->findAll();
+        $data['product'] = $product;
+
+        return view('v_produk', $data);
     }
 }
